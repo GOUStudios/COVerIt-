a360202155872ee991aecfe9a63e3271cf51204d
JuanJoseJ <s301352@polito.studenti.it>
2023-05-30 13:19:37 -0500

    Feat: Add States & Transitions

--- GameDesign/Assets/Scripts/MonoBehaviours/CustomerMonoBehavior.cs
+++ GameDesign/Assets/Scripts/MonoBehaviours/CustomerMonoBehavior.cs
@@ -11,21 +11,45 @@ public class CustomerMonoBehavior : MonoBehaviour, Clickable
     [SerializeField] public int requiredClicks = 1;
     [SerializeField] public int clickTime;
     [SerializeField] public bool wearsMask;
-    [SerializeField] public NPCMovementManager moveManager;
     [SerializeField] public int pointValue;
     [SerializeField] public bool isFrozen = false;
     [SerializeField] public int frozenTimeCount = 0;
 
     [SerializeField] public TaserManager taserManager = TaserManager.Instance;
 
+    [SerializeField] private FiniteStateMachine<CustomerMonoBehavior> fsm;
+
     void Start()
     {
         InvokeRepeating("UnFreeze", 1.0f, 1.0f);
         currentSpeed = baseSpeed;
+
+        fsm = new FiniteStateMachine<CustomerMonoBehavior>(this);
+
+        State unmasked = new Unmasked("Unmasked", this);
+        State masked = new Masked("Masked", this);
+        State frozen = new Frozen("Frozen", this);
+
+        fsm.AddTransition(unmasked, masked, () => wearsMask);
+        fsm.AddTransition(unmasked, frozen, () => isFrozen);
+        fsm.AddTransition(masked, frozen, () => isFrozen);
+        fsm.AddTransition(frozen, unmasked, () => !isFrozen && !wearsMask);
+        fsm.AddTransition(frozen, masked, () => !isFrozen && wearsMask);
+
+        if(wearsMask){
+            fsm.SetState(masked);
+        }else{
+            fsm.SetState(unmasked);
+        }
+    }
+
+    void Update() {
+        fsm.Tik();    
     }
 
     public void Click(ClickType clickType)
     {
+        
         if(clickType == ClickType.LEFT_CLICK)
         {
             clickCunt++;
@@ -50,6 +74,7 @@ public class CustomerMonoBehavior : MonoBehaviour, Clickable
                 }
             }
         }
+        
     }
 
     private void UnFreeze()
@@ -70,5 +95,53 @@ public class CustomerMonoBehavior : MonoBehaviour, Clickable
     bool hasMask()
     {
         return true;
+    }
+
+    private class Unmasked: State{
+        private CustomerMonoBehavior _cmb;
+        public Unmasked(string name, CustomerMonoBehavior cmb) : base(name){
+            _cmb = cmb;
+        }
+
+        public override void Enter(){
+            Debug.Log("Enter unmasked");
+        }
+
+        public override void Tik(){}
+
+        public override void Exit(){}
+
+    }
+
+    private class Masked: State{
+        private CustomerMonoBehavior _cmb;
+        public Masked(string name, CustomerMonoBehavior cmb) : base(name){
+            _cmb = cmb;
+        }
+
+        public override void Enter(){
+            Debug.Log("Enter masked");
+        }
+
+        public override void Tik(){}
+
+        public override void Exit(){}
+
+    }
+
+    private class Frozen: State{
+        private CustomerMonoBehavior _cmb;
+        public Frozen(string name,CustomerMonoBehavior cmb) : base(name){
+            _cmb = cmb;
+        }
+
+        public override void Enter(){
+            Debug.Log("Enter frozen");
+        }
+
+        public override void Tik(){}
+
+        public override void Exit(){}
+
     }
 }
