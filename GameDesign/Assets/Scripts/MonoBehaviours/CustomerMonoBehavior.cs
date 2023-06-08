fafdb98edf3ab629a9d998ca642b20be3e0f2ab6
FABIO CHAVEZ s303519 <fabio.alfredo.fc.fc@gmail.com>
2023-06-08 20:14:54 +0200

    added animations to customerMonobehaviour

--- GameDesign/Assets/Scripts/MonoBehaviours/CustomerMonoBehavior.cs
+++ GameDesign/Assets/Scripts/MonoBehaviours/CustomerMonoBehavior.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(NPCMovementManager))]
 public class CustomerMonoBehavior : MonoBehaviour, Clickable
 {
     [SerializeField] public int id;
@@ -14,20 +15,31 @@ public class CustomerMonoBehavior : MonoBehaviour, Clickable
     [SerializeField] public int pointValue;
     [SerializeField] public bool isFrozen = false;
     [SerializeField] public int frozenTimeCount = 0;
-
     [SerializeField] public TaserManager taserManager = TaserManager.Instance;
-
     [SerializeField] private FiniteStateMachine<CustomerMonoBehavior> fsm;
+    [SerializeField] private NPCMovementManager movementManager;
+    [SerializeField] private Animator animator;
+    public string defaultLayer { get { return "Default"; } }
 
     void Start()
     {
         currentSpeed = baseSpeed;
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+            if (animator == null) Debug.LogError($"Missing Animator component: {name}");
+        }
+        if (movementManager == null)
+        {
+            movementManager = GetComponent<NPCMovementManager>();
+            if (movementManager == null) Debug.LogError($"Missing MovementManager component: {name}");
+        }
 
         fsm = new FiniteStateMachine<CustomerMonoBehavior>(this);
 
         State unmasked = new Unmasked("Unmasked", this);
         State masked = new Masked("Masked", this);
-        State frozen = new Frozen("Frozen", this);
+        State frozen = new Frozen("Frozen", this, animator);
 
         fsm.AddTransition(unmasked, masked, () => wearsMask);
         fsm.AddTransition(unmasked, frozen, () => isFrozen);
@@ -35,24 +47,28 @@ public class CustomerMonoBehavior : MonoBehaviour, Clickable
         fsm.AddTransition(frozen, unmasked, () => !isFrozen && !wearsMask);
         fsm.AddTransition(frozen, masked, () => !isFrozen && wearsMask);
 
-        if(wearsMask){
+        if (wearsMask)
+        {
             fsm.SetState(masked);
-        }else{
+        }
+        else
+        {
             fsm.SetState(unmasked);
         }
     }
 
-    void Update() {
-        fsm.Tik();    
+    void Update()
+    {
+        fsm.Tik();
     }
 
     public void Click(ClickType clickType)
     {
         Debug.Log("RIGHT CLICK");
-        if(clickType == ClickType.LEFT_CLICK)
+        if (clickType == ClickType.LEFT_CLICK)
         {
             clickCunt++;
-            if (!wearsMask  && clickCunt >= requiredClicks)
+            if (!wearsMask && clickCunt >= requiredClicks)
             {
                 wearsMask = true;
                 PointsManager.Instance.TriggerEvent_IncrementPoints(pointValue);
@@ -62,25 +78,50 @@ public class CustomerMonoBehavior : MonoBehaviour, Clickable
                 PointsManager.Instance.TriggerEvent_IncrementPoints(-1 * pointValue);
             }
         }
-        if(clickType == ClickType.RIGHT_CLICK)
+        if (clickType == ClickType.RIGHT_CLICK)
         {
             Debug.Log("LEFT CLICK");
-            if(taserManager.useTaser()){
-                StartCoroutine(StartFreeze(3f)); 
+            if (taserManager.useTaser())
+            {
+                StartCoroutine(StartFreeze(3f));
             }
         }
-        
+
     }
 
-        private IEnumerator StartFreeze(float duration)
+    private IEnumerator StartFreeze(float duration)
     {
-        if (!isFrozen){
-			isFrozen = true;
+        if (!isFrozen)
+        {
+            isFrozen = true;
             yield return new WaitForSeconds(duration);
             isFrozen = false;
-            currentSpeed = baseSpeed;
             Debug.Log("Unfreezed");
         }
+    }
+
+    public void changeSpeed()
+    {
+        changeSpeed(baseSpeed);
+    }
+    public void changeSpeed(float newSpeed)
+    {
+        currentSpeed = newSpeed;
+    }
+
+    public void changeLayer(string LayerName)
+    {
+
+        int Layer = LayerMask.NameToLayer(LayerName);
+        if (Layer < 0)
+        {
+            Debug.LogWarning($"Could not find the expected layer {LayerName}");
+            Layer = 0;
+        }
+
+        gameObject.layer = Layer;
+
+
     }
 
     bool hasMask()
@@ -88,51 +129,74 @@ public class CustomerMonoBehavior : MonoBehaviour, Clickable
         return true;
     }
 
-    private class Unmasked: State{
+    private class Unmasked : State
+    {
         private CustomerMonoBehavior _cmb;
-        public Unmasked(string name, CustomerMonoBehavior cmb) : base(name){
+        public Unmasked(string name, CustomerMonoBehavior cmb) : base(name)
+        {
             _cmb = cmb;
         }
 
-        public override void Enter(){
-            Debug.Log("Enter unmasked");
+        public override void Enter()
+        {
+
         }
 
-        public override void Tik(){}
+        public override void Tik() { }
 
-        public override void Exit(){}
+        public override void Exit()
+        {
+
+        }
 
     }
 
-    private class Masked: State{
+    private class Masked : State
+    {
         private CustomerMonoBehavior _cmb;
-        public Masked(string name, CustomerMonoBehavior cmb) : base(name){
+        public Masked(string name, CustomerMonoBehavior cmb) : base(name)
+        {
             _cmb = cmb;
         }
 
-        public override void Enter(){
+        public override void Enter()
+        {
             Debug.Log("Enter masked");
         }
 
-        public override void Tik(){}
+        public override void Tik() { }
 
-        public override void Exit(){}
+        public override void Exit() { }
 
     }
 
-    private class Frozen: State{
+    private class Frozen : State
+    {
         private CustomerMonoBehavior _cmb;
-        public Frozen(string name,CustomerMonoBehavior cmb) : base(name){
+        private Animator _animator;
+        public Frozen(string name, CustomerMonoBehavior cmb, Animator animator) : base(name)
+        {
             _cmb = cmb;
+            _animator = animator;
         }
 
-        public override void Enter(){
+        public override void Enter()
+        {
+            _cmb.changeLayer("ElectricityLayer");
+            _cmb.changeSpeed(0f);
+            _animator.SetBool("Tazered", true);
             Debug.Log("Enter frozen");
         }
 
-        public override void Tik(){}
+        public override void Tik() { }
 
-        public override void Exit(){}
+        public override void Exit()
+        {
+
+            _cmb.changeLayer(_cmb.defaultLayer);
+            _cmb.changeSpeed();
+            _animator.SetBool("Tazered", false);
+        }
 
     }
 }
