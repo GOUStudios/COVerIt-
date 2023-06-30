c4ce56aeb30c7f017c6c9760151b232a9434a0dd
FABIO CHAVEZ s303519 <fabio.alfredo.fc.fc@gmail.com>
2023-06-30 21:58:48 +0200

    added marco spawner behaviour

---
+++ GameDesign/Assets/Scripts/CharacterScripts/MarcoSpawner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarcoSpawner : MonoBehaviour
+{
+    [SerializeField][Range(0, 100)] float chanceOfSpawnage = 0.001f;
+    void Awake()
+    {
+        if (chanceOfSpawnage > Random.Range(0f, 100f))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
