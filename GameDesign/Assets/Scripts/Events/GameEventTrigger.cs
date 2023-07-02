d1c207acf8d254160d8253efbbece78e2af1a8ba
FABIO CHAVEZ s303519 <fabio.alfredo.fc.fc@gmail.com>
2023-07-02 15:46:02 +0200

    Update req changes 1 + one time trigger
    Scaled slightly the canvas to be a bit smaller and adjusted animations accordingly
    
    added value to playerPrefs to check if the event requires to be played once per save file.

--- GameDesign/Assets/Scripts/Events/GameEventTrigger.cs
+++ GameDesign/Assets/Scripts/Events/GameEventTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public abstract class GameEventTrigger : MonoBehaviour
 {
     [SerializeField] protected bool m_TriggerOnce;
+    [SerializeField] protected bool m_TriggerOncePerGameFile = false;
     protected bool m_hasGameEventBeenTriggered;
     [SerializeField] protected bool m_useEnterinOrExitingHitBox = false;
     [SerializeField] protected GameEvent m_event;
@@ -25,15 +26,17 @@ public abstract class GameEventTrigger : MonoBehaviour
 
     public void TriggerEventViaCode()
     {
-
         if (!m_hasGameEvent) return;
+        if (m_TriggerOncePerGameFile && PlayerPrefs.GetInt(m_event.name, 0) != 0) return;
         if (m_TriggerOnce && m_hasGameEventBeenTriggered) return;
 
         if (EventTriggerCondition())
         {
             m_event.Raise();
             m_hasGameEventBeenTriggered = true;
+            if (m_TriggerOncePerGameFile) PlayerPrefs.SetInt(m_event.name, 1);
             if (m_TriggerOnce) enabled = false;
+
         }
 
     }
