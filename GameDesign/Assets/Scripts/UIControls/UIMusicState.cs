fbd0fbfe52dbea271338dd45afd619353f61b277
Fabio <51410334+theenergici@users.noreply.github.com>
2023-07-11 02:50:44 +0200

    Fix sound (#161)
    * updated vfx and music to work with audio mixer
    
    * made sure ui is consistent between scenes
    
    * update ui
    
    * update prefab UI continue button was blocked
    
    * set audio mixer to default state
    
    * added alarm to mute options

--- GameDesign/Assets/Scripts/UIControls/UIMusicState.cs
+++ GameDesign/Assets/Scripts/UIControls/UIMusicState.cs
@@ -2,22 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
+
 
 
 public class UIMusicState : MonoBehaviour
 {
-    private Toggle toggle;
-  
+    [SerializeField] private Toggle toggle;
+    [SerializeField] AudioMixer audioMixer;
+
 
     // Start is called before the first frame update
     void Start()
     {
-     
-        toggle= GetComponent<Toggle>();
+        if (toggle == null)
+            toggle = GetComponent<Toggle>();
 
         toggle.onValueChanged.AddListener(ChangeStateRequest);
 
-        if(SoundManager.Instance != null )
+        float vol;
+
+        if (audioMixer.GetFloat("VolumeMusic", out vol))
+        {
+            toggle.isOn = vol <= -10f;
+
+            if (toggle.isOn) audioMixer?.SetFloat("VolumeMusic", -80f);
+            else audioMixer?.SetFloat("VolumeMusic", 0);
+        }
+
+        if (SoundManager.Instance != null)
         {
             if (SoundManager.Instance.musicStateRead())
             {
@@ -29,7 +42,21 @@ public class UIMusicState : MonoBehaviour
                 bool newIsOnState = true;
                 toggle.SetIsOnWithoutNotify(newIsOnState);
             }
-                
+
+        }
+    }
+    void Awake()
+    {
+        float vol;
+
+        if (audioMixer.GetFloat("VolumeMusic", out vol))
+        {
+            if (toggle == null) toggle = GetComponent<Toggle>();
+
+            toggle.isOn = vol <= -10.0f;
+
+            if (toggle.isOn) audioMixer?.SetFloat("VolumeMusic", -80f);
+            else audioMixer?.SetFloat("VolumeMusic", 0);
         }
     }
 
@@ -38,6 +65,14 @@ public class UIMusicState : MonoBehaviour
         toggle.onValueChanged.RemoveAllListeners();
     }
 
+    public void toggleAudio()
+    {
+        if (toggle.isOn)
+            audioMixer?.SetFloat("VolumeMusic", -80f);
+        else
+            audioMixer?.SetFloat("VolumeMusic", 0);
+
+    }
 
     private void ChangeStateRequest(bool value)
     {
--- GameDesign/Assets/Scripts/UIControls/UISoundState.cs
+++ GameDesign/Assets/Scripts/UIControls/UISoundState.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
+
 
 public class UISoundState : MonoBehaviour
 {
-    private Toggle toggle;
-
-
+    [SerializeField] private Toggle toggle;
+    [SerializeField] AudioMixer audioMixer;
     // Start is called before the first frame update
     void Start()
     {
+        if (toggle == null)
+            toggle = GetComponent<Toggle>();
 
-        toggle = GetComponent<Toggle>();
+        float vol;
+
+        if (audioMixer.GetFloat("VolumeVFX", out vol))
+        {
+            toggle.isOn = vol <= -10.0f;
+            if (toggle.isOn) audioMixer?.SetFloat("VolumeVFX", -80f);
+            else audioMixer?.SetFloat("VolumeVFX", 0);
+        }
+
 
         toggle.onValueChanged.AddListener(ChangeStateRequest);
 
@@ -30,12 +41,33 @@ public class UISoundState : MonoBehaviour
             }
         }
     }
+    void Awake()
+    {
+        float vol;
+
+        if (audioMixer.GetFloat("VolumeVFX", out vol))
+        {
+            if (toggle == null) toggle = GetComponent<Toggle>();
+            toggle.isOn = vol <= -10.0f;
+            if (toggle.isOn) audioMixer?.SetFloat("VolumeVFX", -80f);
+            else audioMixer?.SetFloat("VolumeVFX", 0);
+        }
+    }
+
 
     private void OnDestroy()
     {
         toggle.onValueChanged.RemoveAllListeners();
     }
 
+    public void toggleAudio()
+    {
+        if (toggle.isOn)
+            audioMixer?.SetFloat("VolumeVFX", -80f);
+        else
+            audioMixer?.SetFloat("VolumeVFX", 0);
+
+    }
 
     private void ChangeStateRequest(bool value)
     {
